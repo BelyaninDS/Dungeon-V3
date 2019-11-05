23adbfac7f7f8780076a1024b0edb6c1433dbcc0
unknown <supersoap78@gmail.com>
2019-11-05 18:05:30 +0300

    Added scripts

---
+++ Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private float dirX, dirY;
+
+    private Rigidbody2D player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        dirY = Input.GetAxis("Vertical");
+
+        player.velocity = new Vector2(dirX * speed, dirY * speed);
+    }
+}
